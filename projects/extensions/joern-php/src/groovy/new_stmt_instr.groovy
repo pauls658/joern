b93dfe7a2eca0116baff9af2a86a92613fa66826
Brandingo <pauls658@d.umn.edu>
2018-04-15 21:14:56 -0700

    idk lol

--- projects/extensions/joern-php/src/groovy/new_stmt_instr.groovy
+++ projects/extensions/joern-php/src/groovy/new_stmt_instr.groovy
@@ -11,9 +11,23 @@ class G {
 	] as HashSet;
 
 	def getForeachInfo(id) {
-		def vars = g.V(id).out('PARENT_OF').has("childnum", within(1,2)).id().toList()
+		def vars = g.V(id).out('PARENT_OF').has("childnum", within(1,2)).not(has("type", "NULL")).id().toList()
 		def stmtlist = g.V(id).out('PARENT_OF').has("childnum", 3).id().toList()[0]
 		return ["stmt":stmtlist, "childnum":-0.5, "vars":vars]
+	}
+
+	def getGlobalInfo(id) {
+		def var = g.V(id).out('PARENT_OF').id().toList()[0]
+		def stmtlistE = g.V(id).repeat(__.inE('PARENT_OF')).until(outV().has("type", "AST_STMT_LIST")).toList()[0]
+		def stmtlist = stmtlistE.outVertex().id()
+		float childnum = Float.parseFloat(stmtlistE.value("child_rel")) + 0.5;
+		return ["stmt":stmtlist, "childnum":childnum, "vars":[var]]
+	}
+
+	def getParamInfo(id) {
+		def var = g.V(id).out('PARENT_OF').has("childnum", 1).id().toList()[0]
+		def stmtlist = g.V(id).in('PARENT_OF').in('PARENT_OF').out('PARENT_OF').has("childnum", 2).id().toList()[0]
+		return ["stmt":stmtlist, "childnum":-0.5, "vars":[var]]
 	}
 
 	def getVarPath(varid) {
@@ -51,6 +65,20 @@ class G {
 		return [leftid, rightid];
 	}
 
+	def makeVarPairsParam(varid) {
+		// In this case, we only have a name
+		def v = g.V(varid).next()
+		def vars = []
+		for (i in [1,2]) {
+			def id = 
+			g.addV("AST").as('child').property("type", "string").property("lineno",0).property("code", v.value("code"))\
+			.addV("AST").property("type", "AST_VAR").property("lineno", 0)\
+			.addE("PARENT_OF").to('child').property("child_rel", "name").outV().id().next()
+			vars += id
+		}
+		return vars
+	}
+
 	def doNewStmtInstr(type, infos) {
 		def scriptDir = new File(getClass().protectionDomain.codeSource.location.path).parent;
 		def outFile = new File("/home/brandon/joern/projects/extensions/joern-php/src/groovy/instrument/" + type + ".csv");
@@ -59,7 +87,13 @@ class G {
 		outFile << "stmtid,childid,leftid,rightid\n";
 		for (info in infos) {
 			for (varid in info["vars"]) {
-				def (left, right) = makeVarPairs(varid);
+				def left
+				def right
+				if (type == "AST_PARAM") {
+					(left, right) = makeVarPairsParam(varid);
+				} else {
+					(left, right) = makeVarPairs(varid);
+				}
 				outFile << info["stmt"] + "," + info["childnum"] + "," + left + "," + right + "\n";
 			}
 		}
@@ -76,6 +110,12 @@ class G {
 		switch (type) {
 			case "AST_FOREACH":
 				infoFunc = this.&getForeachInfo
+				break
+			case "AST_GLOBAL":
+				infoFunc = this.&getGlobalInfo
+				break
+			case "AST_PARAM":
+				infoFunc = this.&getParamInfo
 				break
 			default:
 				throw Exception("Fuck you I'm an exception");
