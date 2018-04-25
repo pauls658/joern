0d1339c85907abf9beffbb4c99829ae02f8d2ffb
Brandingo <pauls658@d.umn.edu>
2018-04-25 16:13:43 -0700

    only func args are def'd now

--- projects/extensions/joern-php/src/main/java/udg/php/useDefAnalysis/environments/FunctionCallEnvironment.java
+++ projects/extensions/joern-php/src/main/java/udg/php/useDefAnalysis/environments/FunctionCallEnvironment.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.LinkedList;
 
 import udg.ASTProvider;
+import udg.ASTNodeASTProvider;
 import udg.useDefAnalysis.environments.EmitDefAndUseEnvironment;
 import udg.useDefGraph.UseOrDef;
 
@@ -14,8 +15,10 @@ public class FunctionCallEnvironment extends EmitDefAndUseEnvironment
 	public void addChildSymbols(LinkedList<String> childSymbols,
 			ASTProvider child)
 	{
-		defSymbols.addAll(childSymbols);
-		useSymbols.addAll(childSymbols);
+		if (isDef(child))
+			defSymbols.addAll(childSymbols);
+		if (isUse(child))
+			useSymbols.addAll(childSymbols);
 	}
 
 	@Override
@@ -33,6 +36,19 @@ public class FunctionCallEnvironment extends EmitDefAndUseEnvironment
 	public LinkedList<String> upstreamSymbols()
 	{
 		return symbols;
+	}
+
+	@Override
+	public boolean isDef( ASTProvider child)
+	{
+		ASTNodeASTProvider c = (ASTNodeASTProvider)child;
+		return c.getASTNode().getProperty("type").equals("AST_ARG_LIST");
+	}
+	
+	@Override
+	public boolean isUse( ASTProvider child)
+	{
+		return true;
 	}
 
 }
